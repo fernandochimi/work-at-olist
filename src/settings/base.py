a6f45c2d35002fc806494ef453e3b0c22d16a827
Fernando Chimicoviaki <fernando.chimicoviaki@d3.do>
2020-02-08 19:04:18 -0300

    Disable Browsable API :triangular_flag_on_post:

--- src/settings/base.py
+++ src/settings/base.py
@@ -83,6 +83,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
     'PAGE_SIZE': 4
 }
 
